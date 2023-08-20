ee1604fa7d909d1c2f2311f548ce08712173bae9
shadownsLuis <141288701+shadownsLuis@users.noreply.github.com>
2023-08-20 15:39:15 -0300

    Add files via upload

---
+++ calaculadora/resut.js
@@ -0,0 +1,24 @@
+let result= document.getElementByld('result');
+
+funcion insertValue(value){
+	result.value
+
+	}
+
+funcion insertValue(value){
+	result.value += value;
+
+}	
+
+funcion deletLast(){
+Result.value = result.value.slic(0,-1);
+}
+
+funcion clearScreen(){
+	result.value =``;
+}
+
+funcion calculate(){
+	result.value=eval(result.value);
+}
+
